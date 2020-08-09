fb152c78498ec8d6fee5d28e1adee5013044b8d9
Quintin Henn <quintin.henn@gmail.com>
2020-08-09 10:53:41 +0200

    Fix generator tests
    Updated Jest and generator test versions

---
+++ __tests__/test.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+
+describe("generator njk: prompting for site name", () => {
+  const componentName = "dummy-site";
+  let tempPath;
+
+  beforeAll((done) => {
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appname: componentName })
+      .on("end", done)
+      .then((result) => {
+        tempPath = result;
+      });
+  });
+
+  it("created base project files", () => {
+    assert.file([
+      `${tempPath}/${componentName}/README.md`,
+      `${tempPath}/${componentName}/package.json`,
+      `${tempPath}/${componentName}/package-lock.json`,
+      `${tempPath}/${componentName}/Gulpfile.js`
+    ]);
+  });
+
+  it("created base dot files", () => {
+    assert.file([
+      `${tempPath}/${componentName}/.editorconfig`,
+      `${tempPath}/${componentName}/.gitattributes`,
+      `${tempPath}/${componentName}/.gitignore`
+    ]);
+  });
+
+  it("created folder structure", () => {
+    assert.file([
+      `${tempPath}/${componentName}/tests/`,
+      `${tempPath}/${componentName}/app/`,
+      `${tempPath}/${componentName}/app/pages/index.njk`,
+      `${tempPath}/${componentName}/app/scss/main.scss`,
+      `${tempPath}/${componentName}/app/static/favicon.ico`
+    ]);
+  });
+});
+
+describe("generator njk: give site name through arguments", () => {
+  const componentName = "dummy-site";
+  let tempPath;
+
+  beforeAll((done) => {
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withArguments(componentName)
+      .on("end", done)
+      .then((result) => {
+        tempPath = result;
+      });
+  });
+
+  it("created project files and folders", () => {
+    assert.file([
+      `${tempPath}/${componentName}/Gulpfile.js`,
+      `${tempPath}/${componentName}/.editorconfig`,
+      `${tempPath}/${componentName}/app/data.json`,
+      `${tempPath}/${componentName}/app/images/.gitkeep`
+    ]);
+  });
+});
